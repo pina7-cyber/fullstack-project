ad2778c23d1d4f189eb788a9deab6abf567950bd
pina7-cyber <patricia.borovski@web.de>
2022-12-17 21:05:23 +0100

    Routing and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Topics from "./components/Topics"
+import NavBar from "./components/NavBar"
+
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <Routes>
+          <Route path='/topics' element={<Topics />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Topics.js
@@ -0,0 +1,5 @@
+const Topics = () => {
+  return <div>Topivs</div>
+}
+
+export default Topics
