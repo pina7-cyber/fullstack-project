2e174d94d0fabc1c405200e1cc16269ac24da1ad
pina7-cyber <patricia.borovski@web.de>
2023-01-22 20:43:32 +0100

    Login functionality

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,14 +1,24 @@
 import { IconButton } from "@mui/material"
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined"
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined"
 
 const Logout = () => {
+  const localToken = window.localStorage.getItem("topixx-user-token")
+
   const handleLogout = (event) => {
-    console.log("logout")
+    console.log("logout or in")
   }
 
+  if (localToken) {
+    return (
+      <IconButton color='inherit' onClick={handleLogout}>
+        <LogoutOutlinedIcon />
+      </IconButton>
+    )
+  }
   return (
     <IconButton color='inherit' onClick={handleLogout}>
-      <LogoutOutlinedIcon />
+      <LoginOutlinedIcon />
     </IconButton>
   )
 }
