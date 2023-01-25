cce7c85c73fbe6b52c2bd5ff326be75de624337d
pina7-cyber <patricia.borovski@web.de>
2023-01-25 20:21:31 +0100

    Account Menu. Not working yet.

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,59 @@
 import { IconButton } from "@mui/material"
-//import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined"
+import { useState } from "react"
+import Box from "@mui/material/Box"
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import ListItemText from "@mui/material/ListItemText"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined"
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined"
 
 const Logout = ({ logout }) => {
+  const [anchorElNav, setAnchorElNav] = useState(null)
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  const menuItems = [
+    { Name: "Logout", Icon: <LogoutOutlinedIcon />, onClick: { logout } },
+  ]
+
   return (
-    <IconButton color='inherit'>
-      <PersonOutlineOutlinedIcon onClick={logout} />
-    </IconButton>
+    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+      <IconButton size='large' onClick={handleOpenNavMenu} color='inherit'>
+        <PersonOutlineOutlinedIcon />
+      </IconButton>
+      <Menu
+        id='menu-appbar'
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+        sx={{
+          display: { xs: "block", md: "none" },
+        }}
+      >
+        {menuItems.map((item) => (
+          <MenuItem key={item.Name} onClick={handleCloseNavMenu}>
+            <ListItemIcon onClick={item.onClick}>{item.Icon}</ListItemIcon>
+            <ListItemText>{item.Name}</ListItemText>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
   )
 }
 
